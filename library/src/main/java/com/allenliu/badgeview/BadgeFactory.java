5fce8447dbb3cae6f47a15a253ae48b532fddeec
gz_liu <29949821@qq.com>
2016-11-08 13:58:16 +0800

    add badgeview shape

--- library/src/main/java/com/allenliu/badgeview/BadgeFactory.java
+++ library/src/main/java/com/allenliu/badgeview/BadgeFactory.java
@@ -1,12 +1,36 @@
 package com.allenliu.badgeview;
 
 import android.content.Context;
+import android.view.Gravity;
+
+import com.allenliu.badgeview.BadgeView;
+
+import static java.security.AccessController.getContext;
 
 /**
  * Created by Allen Liu on 2016/7/15.
  */
 public class BadgeFactory {
+    public static BadgeView createDot(Context context){
+        return  new BadgeView(context).setWidthAndHeight(10,10).setTextSize(0).setBadgeGravity(Gravity.RIGHT| Gravity.TOP).setShape(BadgeView.SHAPE_CIRCLE);
+    }
+    public static BadgeView createCircle(Context context){
+        return  new BadgeView(context).setWidthAndHeight(20,20).setTextSize(12).setBadgeGravity(Gravity.RIGHT| Gravity.TOP).setShape(BadgeView.SHAPE_CIRCLE);
+    }
+    public static BadgeView createRectangle(Context context){
+        return  new BadgeView(context).setWidthAndHeight(25,20).setTextSize(12).setBadgeGravity(Gravity.RIGHT| Gravity.TOP).setShape(BadgeView.SHAPE_RECTANGLE);
+    }
+    public static BadgeView createOval(Context context){
+        return  new BadgeView(context).setWidthAndHeight(25,20).setTextSize(12).setBadgeGravity(Gravity.RIGHT| Gravity.TOP).setShape(BadgeView.SHAPE_OVAL);
+    }
+    public static BadgeView createSquare(Context context){
+        return  new BadgeView(context).setWidthAndHeight(20,20).setTextSize(12).setBadgeGravity(Gravity.RIGHT| Gravity.TOP).setShape(BadgeView.SHAPE_SQUARE);
+    }
+    public static BadgeView createRoundRect(Context context){
+        return  new BadgeView(context).setWidthAndHeight(25,20).setTextSize(12).setBadgeGravity(Gravity.RIGHT| Gravity.TOP).setShape(BadgeView.SHAPTE_ROUND_RECTANGLE);
+    }
     public static BadgeView create(Context context){
         return  new BadgeView(context);
     }
+
 }
--- library/src/main/java/com/allenliu/badgeview/BadgeView.java
+++ library/src/main/java/com/allenliu/badgeview/BadgeView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -26,11 +27,18 @@ public class BadgeView extends View {
      * 背景Paint
      */
     private Paint backgroundPaint;
-    private int defaultTextColor= Color.WHITE;
+    public static final int SHAPE_CIRCLE=1;
+    public static final int SHAPE_RECTANGLE=2;
+    public static final int SHAPE_OVAL=3;
+    public static final int SHAPTE_ROUND_RECTANGLE=4;
+    public static final int SHAPE_SQUARE=5;
+    private  int currentShape=SHAPE_CIRCLE;
+    private int defaultTextColor = Color.WHITE;
     private int defaultTextSize;
-    private int defaultBackgroundColor=Color.RED;
-    private String showText="0";
-    private int badgeGravity=Gravity.RIGHT|Gravity.TOP;
+    private int defaultBackgroundColor = Color.RED;
+    private String showText = "";
+    private int badgeGravity = Gravity.RIGHT | Gravity.TOP;
+
     public BadgeView(Context context) {
         super(context);
         init(context);
@@ -53,139 +61,207 @@ public class BadgeView extends View {
     }
 
     private void init(Context context) {
-        defaultTextSize=dip2px(context,15);
-        numberPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        defaultTextSize = dip2px(context, 1);
+        numberPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         numberPaint.setColor(defaultTextColor);
         numberPaint.setStyle(Paint.Style.FILL);
         numberPaint.setTextSize(defaultTextSize);
         numberPaint.setTextAlign(Paint.Align.CENTER);
-        backgroundPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         backgroundPaint.setColor(defaultBackgroundColor);
         backgroundPaint.setStyle(Paint.Style.FILL);
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
         params.gravity = badgeGravity;
         setLayoutParams(params);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int w=MeasureSpec.getSize(widthMeasureSpec);
-        int h=MeasureSpec.getSize(heightMeasureSpec);
-        int size=w>h?h:w;
-        int measure=MeasureSpec.makeMeasureSpec(size,MeasureSpec.AT_MOST);
-        super.onMeasure(measure, measure);
+        //  int w = MeasureSpec.getSize(widthMeasureSpec);
+        //   int h = MeasureSpec.getSize(heightMeasureSpec);
+        // int size = w > h ? h : w;
+        // int measure = MeasureSpec.makeMeasureSpec(size, MeasureSpec.AT_MOST);
+        // super.onMeasure(measure, measure);
+        super.onMeasure(widthMeasureSpec,heightMeasureSpec);
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(getMeasuredWidth()/2f,getMeasuredHeight()/2f,getMeasuredWidth()/2,backgroundPaint);
-        Paint.FontMetrics fontMetrics=numberPaint.getFontMetrics();
-        float textH=fontMetrics.descent-fontMetrics.ascent;
-        canvas.drawText(showText,getMeasuredWidth()/2f,getMeasuredHeight()/2f+(textH/2f-fontMetrics.descent),numberPaint);
+        RectF rectF=new RectF(0,0,getMeasuredWidth(),getMeasuredHeight());
+        Paint.FontMetrics fontMetrics = numberPaint.getFontMetrics();
+        float textH = fontMetrics.descent - fontMetrics.ascent;
+        switch(currentShape){
+            case SHAPE_CIRCLE:
+                canvas.drawCircle(getMeasuredWidth() / 2f, getMeasuredHeight() / 2f, getMeasuredWidth() / 2, backgroundPaint);
+                canvas.drawText(showText, getMeasuredWidth() / 2f, getMeasuredHeight() / 2f + (textH / 2f - fontMetrics.descent), numberPaint);
+                break;
+            case SHAPE_OVAL:
+
+                canvas.drawOval(rectF,backgroundPaint);
+                canvas.drawText(showText, getMeasuredWidth() / 2f, getMeasuredHeight() / 2f + (textH / 2f - fontMetrics.descent), numberPaint);
+                break;
+            case SHAPE_RECTANGLE:
+                canvas.drawRect(rectF,backgroundPaint);
+                canvas.drawText(showText, getMeasuredWidth() / 2f, getMeasuredHeight() / 2f + (textH / 2f - fontMetrics.descent), numberPaint);
+                break;
+            case SHAPE_SQUARE:
+                int sideLength= Math.min(getMeasuredHeight(),getMeasuredWidth());
+                RectF squareF=new RectF(0,0,sideLength,sideLength);
+                canvas.drawRect(squareF,backgroundPaint);
+                canvas.drawText(showText, sideLength/ 2f, sideLength / 2f + (textH / 2f - fontMetrics.descent), numberPaint);
+                break;
+            case SHAPTE_ROUND_RECTANGLE:
+                canvas.drawRoundRect(rectF,dip2px(getContext(),5),dip2px(getContext(),5),backgroundPaint);
+                canvas.drawText(showText, getMeasuredWidth() / 2f, getMeasuredHeight() / 2f + (textH / 2f - fontMetrics.descent), numberPaint);
+                break;
+        }
+
     }
 
-    private int dip2px(Context context, int dip){
-       return (int) (dip * getContext().getResources().getDisplayMetrics().density + 0.5f);
-   }
+    private int dip2px(Context context, int dip) {
+        return (int) (dip * getContext().getResources().getDisplayMetrics().density + 0.5f);
+    }
+    private  int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
 
+    public BadgeView setShape(int shape){
+        currentShape=shape;
+        invalidate();
+        return this;
+    }
     /**
-     *
-     * @param w  sp
-     * @param h  sp
+     * @param w dip
+     * @param h dip this unit is dip
      * @return
      */
-    public BadgeView setWidthAndHeight(int w,int h){
+    public BadgeView setWidthAndHeight(int w, int h) {
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
-        params.width=dip2px(getContext(),w);
-        params.height=dip2px(getContext(),h);
+        params.width = dip2px(getContext(), w);
+        params.height = dip2px(getContext(), h);
         setLayoutParams(params);
         return this;
     }
 
-    public BadgeView setWidth(int sp){
+    /**
+     *
+     * @param sp dip
+     * @return
+     */
+    public BadgeView setWidth(int sp) {
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
-        params.width=dip2px(getContext(),sp);
+        params.width = dip2px(getContext(), sp);
         setLayoutParams(params);
-        return  this;
+        return this;
 
     }
-    public BadgeView setHeight(int sp){
+
+    public BadgeView setHeight(int sp) {
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
-        params.height=dip2px(getContext(),sp);
+        params.height = dip2px(getContext(), sp);
         setLayoutParams(params);
-        return  this;
+        return this;
     }
 
-    public BadgeView setTextSize(int sp){
-        defaultTextSize=dip2px(getContext(),sp);
-        numberPaint.setTextSize(dip2px(getContext(),sp));
+    /**
+     *
+     * @param sp  the unit is sp
+     * @return
+     */
+    public BadgeView setTextSize(int sp) {
+        defaultTextSize = sp2px(getContext(), sp);
+        numberPaint.setTextSize(sp2px(getContext(), sp));
         invalidate();
-        return  this;
+        return this;
     }
-    public  BadgeView setTextColor(int color){
-        defaultTextColor=color;
+
+    public BadgeView setTextColor(int color) {
+        defaultTextColor = color;
         numberPaint.setColor(color);
         invalidate();
-        return  this;
+        return this;
     }
-    public BadgeView setBadgeBackground(int color){
-        defaultBackgroundColor=color;
+
+    public BadgeView setBadgeBackground(int color) {
+        defaultBackgroundColor = color;
         backgroundPaint.setColor(color);
         invalidate();
-        return  this;
+        return this;
     }
-    public  BadgeView setBadgeCount(int count){
-        showText= String.valueOf(count);
+
+    /**
+     * if shape is dot ,func is not effect
+     * @param count
+     * @return
+     */
+    public BadgeView setBadgeCount(int count) {
+        showText = String.valueOf(count);
         invalidate();
-        return  this;
+        return this;
     }
-    public  BadgeView setBadgeCount(String count){
-        showText= count;
+
+    /**
+     * if shape is dot ,func is not effect
+     * @param count
+     * @return
+     */
+    public BadgeView setBadgeCount(String count) {
+        showText = count;
         invalidate();
-        return  this;
+        return this;
     }
 
     /**
      * set gravity must be before @link bind() method
+     *
      * @param gravity
      * @return
      */
-    public BadgeView setBadgeGravity(int gravity){
-        badgeGravity=gravity;
+    public BadgeView setBadgeGravity(int gravity) {
+        badgeGravity = gravity;
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) getLayoutParams();
         params.gravity = gravity;
         setLayoutParams(params);
-        return  this;
+        return this;
     }
-    public BadgeView bind(View view){
-        if(getParent()!=null)
-            ((ViewGroup) getParent()).removeView(this);
-        if(view==null)
-            return this;
-        if(view.getParent() instanceof FrameLayout){
-            ((FrameLayout) view.getParent()).addView(this);
-        }else if(view.getParent() instanceof  ViewGroup){
-            ViewGroup parentContainer= (ViewGroup) view.getParent();
-            int viewIndex=((ViewGroup) view.getParent()).indexOfChild(view);
-            ((ViewGroup) view.getParent()).removeView(view);
 
-            FrameLayout container=new FrameLayout(getContext());
-            ViewGroup.LayoutParams containerParams=view.getLayoutParams();
+    public BadgeView bind(View view) {
+        if (getParent() != null)
+            ((ViewGroup) getParent()).removeView(this);
+        if (view == null)
+            return this;
+        if (view.getParent() instanceof FrameLayout) {
+            ((FrameLayout) view.getParent()).addView(this);
+        } else if (view.getParent() instanceof ViewGroup) {
+            ViewGroup parentContainer = (ViewGroup) view.getParent();
+            int viewIndex = ((ViewGroup) view.getParent()).indexOfChild(view);
+            ((ViewGroup) view.getParent()).removeView(view);
+            FrameLayout container = new FrameLayout(getContext());
+            ViewGroup.LayoutParams containerParams = view.getLayoutParams();
             container.setLayoutParams(containerParams);
-             container.setId(view.getId());
-            
+            container.setId(view.getId());
             view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             container.addView(view);
             container.addView(this);
-            parentContainer.addView(container,viewIndex);
-        }else if(view.getParent()==null){
-           Log.e("badgeview","View must have a parent");
+            parentContainer.addView(container, viewIndex);
+        } else if (view.getParent() == null) {
+            Log.e("badgeview", "View must have a parent");
         }
-        return  this;
+        return this;
     }
-    public String getBadgeCount(){
+
+    public boolean unbind() {
+        if (getParent() != null) {
+            ((ViewGroup) getParent()).removeView(this);
+            return true;
+        }
+        return false;
+    }
+
+    public String getBadgeCount() {
         return showText;
     }
 }
